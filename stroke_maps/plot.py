e1e23409986a223fc116a33d24fb4a93124ceb5a
Anna Laws <a.laws2@exeter.ac.uk>
2024-06-20 15:58:28 +0100

    Remove empty plot module

--- stroke_maps/plot.py
+++
@@ -1,8 +0,0 @@
-"""
-Draw some maps using GeoDataFrames from the Catchment data.
-
-crs reference:
-+ EPSG:4326  - longitude / latitude.
-+ CRS:84     - same as EPSG:4326.
-+ EPSG:27700 - British National Grid (BNG).
-"""
